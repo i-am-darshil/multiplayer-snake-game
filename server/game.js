123c2498beb006ebb2f353385a5414c1ea01c1b1
Darshil Shah <darshilshah9991@gmail.com>
2022-09-18 23:38:56 +0530

    04 - Add multiplayer handling

--- server/game.js
+++ server/game.js
@@ -13,7 +13,7 @@ function initGame() {
 
 function createGameState() {
   return {
-    players: {
+    players: [{
       pos: {
         x: 3,
         y: 10,
@@ -27,7 +27,21 @@ function createGameState() {
         {x: 2, y: 10},
         {x: 3, y: 10},
       ],
-    },
+    }, {
+      pos: {
+        x: 18,
+        y: 10,
+      },
+      vel: {
+        x: 0,
+        y: 0,
+      },
+      snake: [
+        {x: 20, y: 10},
+        {x: 19, y: 10},
+        {x: 18, y: 10},
+      ],
+    }],
     food: {},
     gridsize: GRID_SIZE,
   };
@@ -38,14 +52,21 @@ function gameLoop(state) {
     return;
   }
 
-  const playerOne = state.players;
+  const playerOne = state.players[0];
+  const playerTwo = state.players[1];
 
   playerOne.pos.x += playerOne.vel.x;
   playerOne.pos.y += playerOne.vel.y;
 
+  playerTwo.pos.x += playerTwo.vel.x;
+  playerTwo.pos.y += playerTwo.vel.y;
 
   if (playerOne.pos.x < 0 || playerOne.pos.x > GRID_SIZE || playerOne.pos.y < 0 || playerOne.pos.y > GRID_SIZE) {
     return 2;
+  }
+
+  if (playerTwo.pos.x < 0 || playerTwo.pos.x > GRID_SIZE || playerTwo.pos.y < 0 || playerTwo.pos.y > GRID_SIZE) {
+    return 1;
   }
 
   if (state.food.x === playerOne.pos.x && state.food.y === playerOne.pos.y) {
@@ -55,6 +76,12 @@ function gameLoop(state) {
     randomFood(state);
   }
 
+  if (state.food.x === playerTwo.pos.x && state.food.y === playerTwo.pos.y) {
+    playerTwo.snake.push({ ...playerTwo.pos });
+    playerTwo.pos.x += playerTwo.vel.x;
+    playerTwo.pos.y += playerTwo.vel.y;
+    randomFood(state);
+  }
 
   if (playerOne.vel.x || playerOne.vel.y) {
     for (let cell of playerOne.snake) {
@@ -67,6 +94,17 @@ function gameLoop(state) {
     playerOne.snake.shift();
   }
 
+  if (playerTwo.vel.x || playerTwo.vel.y) {
+    for (let cell of playerTwo.snake) {
+      if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
+        return 1;
+      }
+    }
+
+    playerTwo.snake.push({ ...playerTwo.pos });
+    playerTwo.snake.shift();
+  }
+
   return false;
 }
 
@@ -76,7 +114,13 @@ function randomFood(state) {
     y: Math.floor(Math.random() * GRID_SIZE),
   }
 
-  for (let cell of state.players.snake) {
+  for (let cell of state.players[0].snake) {
+    if (cell.x === food.x && cell.y === food.y) {
+      return randomFood(state);
+    }
+  }
+
+  for (let cell of state.players[1].snake) {
     if (cell.x === food.x && cell.y === food.y) {
       return randomFood(state);
     }
--- server/index.js
+++ server/index.js
@@ -11,21 +11,76 @@ const io = require('socket.io')(httpServer,{
   })
 const { FRAME_RATE } = require('./constants');
 const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
+const { makeid } = require('./utils');
 
 let state = {};
+let clientRooms = {};
 
 io.on('connection', client => {
   console.log("connection")
-  state = initGame();
+  // state = initGame();
+
   client.on('keydown', handleKeydown);
   client.on('close', (reason) => {
     console.log("Socket closed with reason:", reason)
   })
+  client.on('newGame', handleNewGame);
+  client.on('joinGame', handleJoinGame);
+
   client.emit('init', {data: "Initializing Game"})
-  startGameInterval(client);
+
+  function handleJoinGame(roomName) {
+    const room = io.of("/").adapter.rooms.get(roomName);
+
+    let numClients = 0;
+    if (room) {
+      numClients = room.size
+    }
+    console.log("roomName, room, numClients", roomName, room,numClients)
+    
+
+    if (numClients === 0) {
+      client.emit('unknownCode');
+      return;
+    } else if (numClients > 1) {
+      client.emit('tooManyPlayers');
+      return;
+    }
+
+    client.emit('gameCode', roomName);
+
+
+    clientRooms[client.id] = roomName;
+
+    client.join(roomName);
+    client.number = 2;
+    client.emit('init', 2);
+    
+    startGameInterval(roomName, client);
+  }
+
+  
+  function handleNewGame() {
+    let roomName = makeid(5);
+    clientRooms[client.id] = roomName;
+    client.emit('gameCode', roomName);
+
+    state[roomName] = initGame();
+
+    client.join(roomName);
+    console.log("handleNewGame : roomName, io.sockets.adapter.rooms[roomName]", roomName, io.of("/").adapter.rooms.get(roomName))
+
+    client.number = 1;
+    client.emit('init', 1);
+  }
+
+
 
   function handleKeydown(keyCode) {
-
+    const roomName = clientRooms[client.id];
+    if (!roomName) {
+      return;
+    }
     try {
       keyCode = parseInt(keyCode);
     } catch(e) {
@@ -36,21 +91,33 @@ io.on('connection', client => {
     const vel = getUpdatedVelocity(keyCode);
 
     if (vel) {
-      state.players.vel = vel;
+      state[roomName].players[client.number - 1].vel = vel;
     }
   }
 });
 
-function startGameInterval(client) {
+function startGameInterval(roomName, client) {
   const intervalId = setInterval(() => {
-    let winner = gameLoop(state);
+    let winner = gameLoop(state[roomName]);
     if (!winner) {
-      client.emit('gameState', JSON.stringify(state));
+      emitGameState(roomName, state[roomName], client)
     } else {
+      emitGameOver(roomName, winner, client);
+      state[roomName] = null;
       clearInterval(intervalId);
-      client.emit('gameOver', JSON.stringify({msg: "Game over"}));
     }
   }, 1000 / FRAME_RATE);
+}
+
+function emitGameState(room, gameState, client) {
+  // Send this event to everyone in the room.
+  io.sockets.in(room)
+    .emit('gameState', JSON.stringify(gameState));
+}
+
+function emitGameOver(room, winner,client) {
+  io.sockets.in(room)
+    .emit('gameOver', JSON.stringify({ winner }));
 }
 
 
